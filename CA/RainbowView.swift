77351425692760b94feabf6b2cce5e7dd645692e
Ali Topcu <atopcu365@gmail.com>
2017-04-05 01:42:40 -0400

    Project is created

---
+++ CA/RainbowView.swift
@@ -0,0 +1,21 @@
+//
+//  RainbowView.swift
+//  CA
+//
+//  Created by Ali Topcu on 4/5/17.
+//  Copyright © 2017 Ali Topcu. All rights reserved.
+//
+
+import UIKit
+
+class RainbowView: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
---
+++ CA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CA
+//
+//  Created by Ali Topcu on 4/3/17.
+//  Copyright © 2017 Ali Topcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
